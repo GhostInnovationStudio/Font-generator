27bf632440b93e7da717a9b835c56ad004a5dd23
沉入代码的幽灵 <15613786704@139.com>
2024-11-10 19:59:14 +0800

    Create Font generator.cpp

---
+++ Font generator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+/*
+这个主要是进行一个字体的转换，所以说我们先定义一个空函数，目前不更新
+*/
+string Add(string text,string t)//text是将要转换的英文文本，t是转换类型
+{
+  string a;
+  //中间是转换过程我就不写了
+  return a;
+}
+int main()
+{
+  //一些大概逻辑，以后会运用windowsAPI
+  string a,b;
+  cout<<"请输入你想要转换的文本：";
+  cin>>a;
+  cout<<"请输入你想要转换的文本类型："
+  cin>>b;
+  Add(a,b);
+  return 0;
+}
